8ad44e1cf0732136a69b9730e345a08711778d89
CorvoElefante <corvoelefante@gmail.com>
2017-01-24 14:47:46 -0300

    Criação das models de funcionario, produto e serviço

---
+++ Carro/Carro/Carro/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace Carro.Models
+{
+    public partial class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public float  Preco { get; set; }//Preco de venda do produto
+
+        public string Marca { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Local { get; set; }//Localizacao do produto na loja(estante, balcao, mostruario...)
+
+        public string Custo { get; set; }//Preco pago para obtenção do produto
+    }
+}
