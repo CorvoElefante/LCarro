2668c66441bb8b8ee7d4ac29d08f1304b2a7f5f9
Pinho <jppinho1@gmail.com>
2017-02-06 14:59:24 -0300

    Icones adicionados

--- Carro/Carro/Carro/Resources/image.cs
+++ Carro/Carro/Carro/Resources/image.cs
@@ -5,6 +5,12 @@ namespace Carro.Resources
 {
     public static class Images
     {
-        public static FileImageSource IconeAdicionar = new FileImageSource { File = "ic_add" };
+        public static FileImageSource IconeAdicionar = new FileImageSource { File = "ic_adicionar" };
+        public static FileImageSource IconeAdicionarPessoa = new FileImageSource { File = "ic_adicionarPessoa" };
+        public static FileImageSource IconeCerto= new FileImageSource { File = "ic_certo" };
+        public static FileImageSource IconeInfo = new FileImageSource { File = "ic_info" };
+        public static FileImageSource IconePessoa = new FileImageSource { File = "ic_pessoa" };
+        public static FileImageSource IconeRelatorio = new FileImageSource { File = "ic_relatorio" };
+        public static FileImageSource IconeRelogio = new FileImageSource { File = "ic_relogio" };
     }
 }
