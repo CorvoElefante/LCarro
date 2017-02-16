3ca41c02bf79eb240340ecb218cb6240db5dea7c
CorvoElefante <corvoelefante@gmail.com>
2017-02-16 18:45:56 -0300

    Alterado o nome da page FuncionarioPage para CadastroFuncionarioPage
    Adicionados propriedades a viewmodel do funcionario
    
    Implementação parcial da lista de funcionário

--- Carro/Carro/Carro/Pages/CadastroFuncionarioPage.xaml.cs
+++ Carro/Carro/Carro/Pages/CadastroFuncionarioPage.xaml.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Carro.ViewModels;
 using Xamarin.Forms;
 
 namespace Carro.Pages
 {
-    public partial class FuncionarioPage : ContentPage
+    public partial class CadastroFuncionarioPage : ContentPage
     {
-        public FuncionarioPage()
+        public CadastroFuncionarioPage()
         {
+            BindingContext = new FuncionarioViewModel(Navigation);
             InitializeComponent();
         }
     }
