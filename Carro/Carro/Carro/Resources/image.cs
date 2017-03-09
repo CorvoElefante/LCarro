f62428cbdbbe50813ce944ad430a7ecb55309580
Pinho <jppinho1@gmail.com>
2017-03-09 16:42:45 -0400

    Adicionado novos icones.

--- Carro/Carro/Carro/Resources/image.cs
+++ Carro/Carro/Carro/Resources/image.cs
@@ -5,18 +5,43 @@ namespace Carro.Resources
 {
     public static class Images
     {
-        public static FileImageSource IconeAnalytics = new FileImageSource { File = "ic_analytics" };
-        public static FileImageSource IconeCar = new FileImageSource { File = "ic_car" };
-        public static FileImageSource IconeCreditcard = new FileImageSource { File = "ic_creditcard" };
-        public static FileImageSource IconeEducation = new FileImageSource { File = "ic_education" };
-        public static FileImageSource IconeMan = new FileImageSource { File = "ic_man" };
-        public static FileImageSource IconeTag = new FileImageSource { File = "ic_tag" };
-        public static FileImageSource IconeTeam = new FileImageSource { File = "ic_team" };
-        public static FileImageSource IconeTimeIsMoney = new FileImageSource { File = "ic_timeismoney" };
-        public static FileImageSource IconeWrench = new FileImageSource { File = "ic_wrench" };
+        public static FileImageSource IconeAdd32 = new FileImageSource { File = "ic_add32" };
+        public static FileImageSource IconeError32 = new FileImageSource { File = "ic_error32" };
+        public static FileImageSource IconeInfo32 = new FileImageSource { File = "ic_info32" };
+        public static FileImageSource IconeSucess32 = new FileImageSource { File = "ic_success32" };
+        public static FileImageSource IconeUser132 = new FileImageSource { File = "ic_user132" };
+        public static FileImageSource IconeUser232 = new FileImageSource { File = "ic_user232" };
+        public static FileImageSource IconeUser332 = new FileImageSource { File = "ic_user332" };
+        public static FileImageSource IconeUser32 = new FileImageSource { File = "ic_user32" };
+        public static FileImageSource IconeAnalytics32 = new FileImageSource { File = "ic_analytics32" };
+        public static FileImageSource IconeCreditcard32 = new FileImageSource { File = "ic_creditcard32" };
+        public static FileImageSource IconeEducation32 = new FileImageSource { File = "ic_education32" };
+        public static FileImageSource IconeMan32 = new FileImageSource { File = "ic_man32" };
+        public static FileImageSource IconeTag32 = new FileImageSource { File = "ic_tag32" };
+        public static FileImageSource IconeTeam32 = new FileImageSource { File = "ic_team32" };
+        public static FileImageSource IconeTimeIsMoney32 = new FileImageSource { File = "ic_timeismoney32" };
+        public static FileImageSource IconeWrench32 = new FileImageSource { File = "ic_wrench32" };
+
+        public static FileImageSource IconeAdd128 = new FileImageSource { File = "ic_add128" };
+        public static FileImageSource IconeError128 = new FileImageSource { File = "ic_error128" };
+        public static FileImageSource IconeInfo128 = new FileImageSource { File = "ic_info128" };
+        public static FileImageSource IconeSucess128 = new FileImageSource { File = "ic_success128" };
+        public static FileImageSource IconeUser1128 = new FileImageSource { File = "ic_user1128" };
+        public static FileImageSource IconeUser2128 = new FileImageSource { File = "ic_user2128" };
+        public static FileImageSource IconeUser3128 = new FileImageSource { File = "ic_user3128" };
+        public static FileImageSource IconeUser128 = new FileImageSource { File = "ic_user128" };
+        public static FileImageSource IconeAnalytics128 = new FileImageSource { File = "ic_analytics128" };
+        public static FileImageSource IconeCreditcard128 = new FileImageSource { File = "ic_creditcard128" };
+        public static FileImageSource IconeEducation128 = new FileImageSource { File = "ic_education" };
+        public static FileImageSource IconeMan128 = new FileImageSource { File = "ic_man128" };
+        public static FileImageSource IconeTag128 = new FileImageSource { File = "ic_tag128" };
+        public static FileImageSource IconeTeam128 = new FileImageSource { File = "ic_team128" };
+        public static FileImageSource IconeTimeIsMoney128 = new FileImageSource { File = "ic_timeismoney128" };
+        public static FileImageSource IconeWrench128 = new FileImageSource { File = "ic_wrench128" };
+
         public static FileImageSource IconeAdicionar = new FileImageSource { File = "ic_adicionar" };
         public static FileImageSource IconeAdicionarPessoa = new FileImageSource { File = "ic_adicionarPessoa" };
-        public static FileImageSource IconeCerto= new FileImageSource { File = "ic_certo" };
+        public static FileImageSource IconeCerto = new FileImageSource { File = "ic_certo" };
         public static FileImageSource IconePessoa = new FileImageSource { File = "ic_pessoa" };
     }
 }
