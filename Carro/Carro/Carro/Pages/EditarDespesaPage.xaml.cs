388e312b6300223403ee5a71eb66d9e5608ef467
Pinho <jppinho1@gmail.com>
2017-03-13 15:48:05 -0400

    "EditarDespesaPage" e "EditarPerdaPage" criados.

---
+++ Carro/Carro/Carro/Pages/EditarDespesaPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Carro.ViewModels;
+using Carro.Models;
+
+namespace Carro.Pages
+{
+    public partial class EditarDespesaPage : ContentPage
+    {
+        public EditarDespesaPage(/*Despesa value*/)
+        {
+            //BindingContext = new EditarDespesaViewModel(Navigation, value);
+            InitializeComponent();
+        }
+    }
+}
---
+++ Carro/Carro/Carro/Pages/EditarPerdaPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Carro.ViewModels;
+using Carro.Models;
+
+namespace Carro.Pages
+{
+    public partial class EditarPerdaPage : ContentPage
+    {
+        public EditarPerdaPage(/*Perda Value*/)
+        {
+            //BindingContext = new EditarPerdaViewModel(Navigation, value);
+            InitializeComponent();
+        }
+    }
+}
