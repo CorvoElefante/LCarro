e561f9c8d28b94ae6a4771ae23f95d7d11d0a558
CorvoElefante <corvoelefante@gmail.com>
2017-03-15 17:50:18 -0400

    Edicao parcial das ViewModel relacionadas a Perda

--- Carro/Carro/Carro/ViewModels/ListaPerdaViewModel.cs
+++ Carro/Carro/Carro/ViewModels/ListaPerdaViewModel.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using Carro.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Carro.Repositories;
+using Carro.Services;
 using System.Threading.Tasks;
+using Carro.Pages;
 
 namespace Carro.ViewModels
 {
-    class ListaPerdaViewModel
+    public class ListaPerdaViewModel : BaseViewModel
     {
+
+        public ListaPerdaViewModel(INavigation navigation) : base(navigation)
+        {
+
+            var sqlite = DependencyService.Get<ISQLite>();
+
+        }
+
+        ObservableCollection<Perda> _Perdas;
+        public ObservableCollection<Perda> Perdas
+        {
+            get
+            {
+                return _Perdas;
+            }
+            set
+            {
+                _Perdas = value;
+                SetPropertyChanged(nameof(Perdas));
+            }
+        }
+
+        string _Search = string.Empty;
+        public string Search
+        {
+            get { return _Search; }
+            set
+            {
+                _Search = value;
+                var sqlite = DependencyService.Get<ISQLite>();
+                Perdas = new ObservableCollection<Perda>(new DataService(sqlite).FindPerdaByNome(_Search));
+            }
+        }
+
+        Command _AddPerdaCommand;
+        public Command AddPerdaCommand
+        {
+            get { return _AddPerdaCommand ?? (_AddPerdaCommand = new Command(async () => await ExecuteAddPerdaCommand())); }
+        }
+
+        async Task ExecuteAddPerdaCommand()
+        {
+            if (!IsBusy)
+            {
+                IsBusy = true;
+                await Navigation.PushAsync(new CadastroPerdaPage());
+                IsBusy = false;
+            }
+        }
+
     }
 }
