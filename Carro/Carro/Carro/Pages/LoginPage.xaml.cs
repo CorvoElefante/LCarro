d21e792b2080e9d2c384aa9f354a34f981e84a14
Pinho <jppinho1@gmail.com>
2017-03-15 23:27:16 -0400

    LoginPage criado.

---
+++ Carro/Carro/Carro/Pages/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Carro.ViewModels;
+
+namespace Carro.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            BindingContext = new LoginViewModel(Navigation);
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
---
+++ Carro/Carro/Carro/ViewModels/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using Carro.Models;
+using System.Collections.Generic;
+using Carro.Repositories;
+using Carro.Services;
+using System.Threading.Tasks;
+using Carro.Pages;
+
+
+namespace Carro.ViewModels
+{
+    class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel(INavigation navigation) : base(navigation)
+        {
+        }
+
+        Command _MenuCommand;
+        public Command MenuCommand
+        {
+            get { return _MenuCommand ?? (_MenuCommand = new Command(async () => await ExecuteMenuCommand())); }
+        }
+
+        async Task ExecuteMenuCommand()
+        {
+            if (!IsBusy)
+            {
+                IsBusy = true;
+                await Navigation.PushAsync(new MenuPage());
+                IsBusy = false;
+            }
+        }
+    }
+}
