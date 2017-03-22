0dd5bc4fa423687f3c5ece83a8cb1fbec110c5c6
CorvoElefante <corvoelefante@gmail.com>
2017-03-22 16:56:00 -0400

    Adicionada a propriedade quantidadeAdicionadaEntry na EditarProdutoViewModel

--- Carro/Carro/Carro/ViewModels/EditarProdutoViewModel.cs
+++ Carro/Carro/Carro/ViewModels/EditarProdutoViewModel.cs
@@ -141,6 +141,20 @@ namespace Carro.ViewModels
             }
         }
 
+        int _quantidadeAdicionadaEntry = 0;
+        public int quantidadeAdicionadaEntry
+        {
+            get
+            {
+                return _quantidadeAdicionadaEntry;
+            }
+            set
+            {
+                _quantidadeAdicionadaEntry = value;
+                SetPropertyChanged(nameof(quantidadeAdicionadaEntry));
+            }
+        }
+
         string _marcaEntry = string.Empty;
         public string marcaEntry
         {
@@ -202,7 +216,7 @@ namespace Carro.ViewModels
                     {
                         var service = new DataService(sqlite);
 
-                        service.SaveProduto(new Produto { Id = idEntry, Nome = nomeEntry, Preco = precoEntry, Quantidade = quantidadeEntry, Marca = marcaEntry, Descricao = descricaoEntry, Local = localEntry });
+                        service.SaveProduto(new Produto { Id = idEntry, Nome = nomeEntry, Preco = precoEntry, Quantidade = (quantidadeEntry + quantidadeAdicionadaEntry), Marca = marcaEntry, Descricao = descricaoEntry, Local = localEntry });
                         scope.Complete();
                     }
                     await Navigation.PopAsync();
