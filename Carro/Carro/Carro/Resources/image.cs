6db0b94e4b2e2eed167cc0c59c2472fb8f8b4d8b
CorvoElefante <corvoelefante@gmail.com>
2017-05-24 17:38:00 -0400

    Adiciona os icones do menu principal corretos e edita o arquivo de imagens

--- Carro/Carro/Carro/Resources/image.cs
+++ Carro/Carro/Carro/Resources/image.cs
@@ -5,64 +5,17 @@ namespace Carro.Resources
 {
     public static class Images
     {
-        public static FileImageSource IconeAdd32 = new FileImageSource { File = "ic_add32" };
-        public static FileImageSource IconeError32 = new FileImageSource { File = "ic_error32" };
-        public static FileImageSource IconeInfo32 = new FileImageSource { File = "ic_info32" };
-        public static FileImageSource IconeSucess32 = new FileImageSource { File = "ic_success32" };
-        public static FileImageSource IconeUser132 = new FileImageSource { File = "ic_user132" };
-        public static FileImageSource IconeUser232 = new FileImageSource { File = "ic_user232" };
-        public static FileImageSource IconeUser332 = new FileImageSource { File = "ic_user332" };
-        public static FileImageSource IconeUser32 = new FileImageSource { File = "ic_user32" };
-        public static FileImageSource IconeAnalytics32 = new FileImageSource { File = "ic_analytics32" };
-        public static FileImageSource IconeCreditcard32 = new FileImageSource { File = "ic_creditcard32" };
-        public static FileImageSource IconeEducation32 = new FileImageSource { File = "ic_education32" };
-        public static FileImageSource IconeMan32 = new FileImageSource { File = "ic_man32" };
-        public static FileImageSource IconeTag32 = new FileImageSource { File = "ic_tag32" };
-        public static FileImageSource IconeTeam32 = new FileImageSource { File = "ic_team32" };
-        public static FileImageSource IconeTimeIsMoney32 = new FileImageSource { File = "ic_timeismoney32" };
-        public static FileImageSource IconeServices32 = new FileImageSource { File = "ic_services32" };
-        public static FileImageSource IconeProdutos32 = new FileImageSource { File = "ic_cart32" };
-        public static FileImageSource IconeShield32 = new FileImageSource { File = "ic_shield32" };
-        public static FileImageSource IconeBusiness32 = new FileImageSource { File = "ic_business32" };
-        public static FileImageSource IconeBuy32 = new FileImageSource { File = "ic_buy32" };
 
-        public static FileImageSource IconeAdd128 = new FileImageSource { File = "ic_add128" };
-        public static FileImageSource IconeError128 = new FileImageSource { File = "ic_error128" };
-        public static FileImageSource IconeInfo128 = new FileImageSource { File = "ic_info128" };
-        public static FileImageSource IconeSucess128 = new FileImageSource { File = "ic_success128" };
-        public static FileImageSource IconeUser1128 = new FileImageSource { File = "ic_user1128" };
-        public static FileImageSource IconeUser2128 = new FileImageSource { File = "ic_user2128" };
-        public static FileImageSource IconeUser3128 = new FileImageSource { File = "ic_user3128" };
-        public static FileImageSource IconeUser128 = new FileImageSource { File = "ic_user128" };
-        public static FileImageSource IconeAnalytics128 = new FileImageSource { File = "ic_analytics128" };
-        public static FileImageSource IconeCreditcard128 = new FileImageSource { File = "ic_creditcard128" };
-        public static FileImageSource IconeEducation128 = new FileImageSource { File = "ic_education" };
-        public static FileImageSource IconeMan128 = new FileImageSource { File = "ic_man128" };
-        public static FileImageSource IconeTag128 = new FileImageSource { File = "ic_tag128" };
-        public static FileImageSource IconeTeam128 = new FileImageSource { File = "ic_team128" };
-        public static FileImageSource IconeTimeIsMoney128 = new FileImageSource { File = "ic_timeismoney128" };
-        public static FileImageSource IconeServices128 = new FileImageSource { File = "ic_services128" };
-        public static FileImageSource IconeProdutos128 = new FileImageSource { File = "ic_cart128" };
-        public static FileImageSource IconeShield128 = new FileImageSource { File = "ic_shield128" };
-        public static FileImageSource IconeBusiness128 = new FileImageSource { File = "ic_business128" };
-        public static FileImageSource IconeBuy128 = new FileImageSource { File = "ic_buy128" };
-        public static FileImageSource IconeMoney128 = new FileImageSource { File = "ic_money128" };
-
-        public static FileImageSource IconeAdicionarPessoa = new FileImageSource { File = "ic_adicionarPessoa" };
-        public static FileImageSource IconeCerto = new FileImageSource { File = "ic_certo" };
-        public static FileImageSource IconePessoa = new FileImageSource { File = "ic_pessoa" };
-
-        public static FileImageSource ic_Cliente_48x = new FileImageSource { File = "ic_Cliente_48x" };
-        public static FileImageSource ic_Cliente_teste = new FileImageSource { File = "ic_Cliente_teste" };
-        public static FileImageSource ic_Despesa_48x = new FileImageSource { File = "ic_Despesa_48x" };
-        public static FileImageSource ic_Funcionario_48x = new FileImageSource { File = "ic_Funcionario_48x" };
-        public static FileImageSource ic_Opcoes_48x = new FileImageSource { File = "ic_Opcoes_48x" };
-        public static FileImageSource ic_Perda_48x = new FileImageSource { File = "ic_Perda_48x" };
-        public static FileImageSource ic_PreVenda_48x = new FileImageSource { File = "ic_PreVenda_48x" };
-        public static FileImageSource ic_Produto_48x = new FileImageSource { File = "ic_Produto_48x" };
-        public static FileImageSource ic_Relatorio_48x = new FileImageSource { File = "ic_Relatorio_48x" };
-        public static FileImageSource ic_Servico_48x = new FileImageSource { File = "ic_Servico_48x" };
-        public static FileImageSource ic_Venda_48x = new FileImageSource { File = "ic_Venda_48x" };
+        public static FileImageSource ic_Cliente_Large = new FileImageSource { File = "ic_Cliente_Large" };
+        public static FileImageSource ic_Despesa_Large = new FileImageSource { File = "ic_Despesa_Large" };
+        public static FileImageSource ic_Funcionario_Large = new FileImageSource { File = "ic_Funcionario_Large" };
+        public static FileImageSource ic_Opcao_Large = new FileImageSource { File = "ic_Opcao_Large" };
+        public static FileImageSource ic_Perda_Large = new FileImageSource { File = "ic_Perda_Large" };
+        public static FileImageSource ic_PreVenda_Large = new FileImageSource { File = "ic_PreVenda_Large" };
+        public static FileImageSource ic_Produto_Large = new FileImageSource { File = "ic_Produto_Large" };
+        public static FileImageSource ic_Relatorio_Large = new FileImageSource { File = "ic_Relatorio_Large" };
+        public static FileImageSource ic_Servico_Large = new FileImageSource { File = "ic_Servico_Large" };
+        public static FileImageSource ic_Venda_Large = new FileImageSource { File = "ic_Venda_Large" };
 
         public static FileImageSource ic_Cliente_24x = new FileImageSource { File = "ic_Cliente_24x" };
         public static FileImageSource ic_Despesa_24x = new FileImageSource { File = "ic_Despesa_24x" };
