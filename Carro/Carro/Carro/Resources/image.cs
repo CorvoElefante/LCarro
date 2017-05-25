a8b51b96e8ad3b9037ea0297238bab8dedbd060b
Pinho <jppinho1@gmail.com>
2017-05-24 21:52:44 -0400

    Adicionado todos os icones do menu de relatorios ("Large "e "Small").

--- Carro/Carro/Carro/Resources/image.cs
+++ Carro/Carro/Carro/Resources/image.cs
@@ -5,7 +5,7 @@ namespace Carro.Resources
 {
     public static class Images
     {
-
+        #region Menu
         public static FileImageSource ic_Cliente_Large = new FileImageSource { File = "ic_Cliente_Large" };
         public static FileImageSource ic_Despesa_Large = new FileImageSource { File = "ic_Despesa_Large" };
         public static FileImageSource ic_Funcionario_Large = new FileImageSource { File = "ic_Funcionario_Large" };
@@ -16,16 +16,41 @@ namespace Carro.Resources
         public static FileImageSource ic_Relatorio_Large = new FileImageSource { File = "ic_Relatorio_Large" };
         public static FileImageSource ic_Servico_Large = new FileImageSource { File = "ic_Servico_Large" };
         public static FileImageSource ic_Venda_Large = new FileImageSource { File = "ic_Venda_Large" };
+        #endregion
 
+        #region Lista
         public static FileImageSource ic_Cliente_Small = new FileImageSource { File = "ic_Cliente_Small" };
         public static FileImageSource ic_Despesa_Small = new FileImageSource { File = "ic_Despesa_Small" };
         public static FileImageSource ic_Funcionario_Small = new FileImageSource { File = "ic_Funcionario_Small" };
-        public static FileImageSource ic_Opcao_Small = new FileImageSource { File = "ic_Opcoes_Small" };
+        public static FileImageSource ic_Opcao_Small = new FileImageSource { File = "ic_Opcao_Small" };
         public static FileImageSource ic_Perda_Small = new FileImageSource { File = "ic_Perda_Small" };
         public static FileImageSource ic_PreVenda_Small = new FileImageSource { File = "ic_PreVenda_Small" };
         public static FileImageSource ic_Produto_Small = new FileImageSource { File = "ic_Produto_Small" };
         public static FileImageSource ic_Relatorio_Small = new FileImageSource { File = "ic_Relatorio_Small" };
         public static FileImageSource ic_Servico_Small = new FileImageSource { File = "ic_Servico_Small" };
         public static FileImageSource ic_Venda_Small = new FileImageSource { File = "ic_Venda_Small" };
+        #endregion
+
+        #region Menu_Relatorio
+        public static FileImageSource ic_Relatorio_Cliente_Large = new FileImageSource { File = "ic_Relatorio_Cliente_Large" };
+        public static FileImageSource ic_Relatorio_Despesa_Large = new FileImageSource { File = "ic_Relatorio_Despesa_Large" };
+        public static FileImageSource ic_Relatorio_Funcionario_Large = new FileImageSource { File = "ic_Relatorio_Funcionario_Large" };
+        public static FileImageSource ic_Relatorio_Perda_Large = new FileImageSource { File = "ic_Relatorio_Perda_Large" };
+        public static FileImageSource ic_Relatorio_PreVenda_Large = new FileImageSource { File = "ic_Relatorio_PreVenda_Large" };
+        public static FileImageSource ic_Relatorio_Produto_Large = new FileImageSource { File = "ic_Relatorio_Produto_Large" };
+        public static FileImageSource ic_Relatorio_Servico_Large = new FileImageSource { File = "ic_Relatorio_Servico_Large" };
+        public static FileImageSource ic_Relatorio_Venda_Large = new FileImageSource { File = "ic_Relatorio_Venda_Large" };
+        #endregion
+
+        #region Lista_Relatorio
+        public static FileImageSource ic_Relatorio_Cliente_Small = new FileImageSource { File = "ic_Relatorio_Cliente_Small" };
+        public static FileImageSource ic_Relatorio_Despesa_Small = new FileImageSource { File = "ic_Relatorio_Despesa_Small" };
+        public static FileImageSource ic_Relatorio_Funcionario_Small = new FileImageSource { File = "ic_Relatorio_Funcionario_Small" };
+        public static FileImageSource ic_Relatorio_Perda_Small = new FileImageSource { File = "ic_Relatorio_Perda_Small" };
+        public static FileImageSource ic_Relatorio_PreVenda_Small = new FileImageSource { File = "ic_Relatorio_PreVenda_Small" };
+        public static FileImageSource ic_Relatorio_Produto_Small = new FileImageSource { File = "ic_Relatorio_Produto_Small" };
+        public static FileImageSource ic_Relatorio_Servico_Small = new FileImageSource { File = "ic_Relatorio_Servico_Small" };
+        public static FileImageSource ic_Relatorio_Venda_Small = new FileImageSource { File = "ic_Relatorio_Venda_Small" };
+        #endregion
     }
 }
