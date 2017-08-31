31769f69a37f648908802c4723eca1db15d45d48
CorvoElefante <corvoelefante@gmail.com>
2017-08-31 17:56:59 -0400

    Criadas as paginas de relatorios de final venda e saldo e navegação até as mesmas

---
+++ Carro/Carro/Carro/ViewModels/Relatorios/RelatorioMenuSaldoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using Carro.Models;
+using System.Collections.Generic;
+using Carro.Repositories;
+using Carro.Services;
+using System.Threading.Tasks;
+using Carro.Pages.Relatorios;
+
+namespace Carro.ViewModels.Relatorios
+{
+    public class RelatorioMenuSaldoViewModel : BaseViewModel
+    {
+        public RelatorioMenuSaldoViewModel(INavigation navigation) : base(navigation)
+        {
+        }
+
+        Command _RelClienteAddCommand;
+        public Command RelClienteAddCommand
+        {
+            get { return _RelClienteAddCommand ?? (_RelClienteAddCommand = new Command(async () => await ExecuteRelClienteAddCommand())); }
+        }
+
+        async Task ExecuteRelClienteAddCommand()
+        {
+            if (!IsBusy)
+            {
+                IsBusy = true;
+                await Navigation.PushAsync(new RelatorioClienteAdicionadoSelecaoPage());
+                IsBusy = false;
+            }
+        }
+    }
+}
