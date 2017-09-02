622622387a4fbacfea087e5de77fd341d9a2fbef
jpcc <jppinho1@gmail.com>
2017-09-01 20:42:26 -0400

    Outro bug arrumado no command da page sobre.

--- Carro/Carro/Carro/Pages/OpcaoSobrePage.xaml.cs
+++ Carro/Carro/Carro/Pages/OpcaoSobrePage.xaml.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Carro.ViewModels;
+
 
 namespace Carro.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class OpcaoSobrePage : ContentPage
 	{
 		public OpcaoSobrePage ()
